3d96f3d0e2ada97e7e4edd249f95f93d86f82f47
Miseda <misedaalex@gmail.com>
2019-02-22 12:38:27 +0300

    Added parse int and funtion to the jquewry.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
  $("form#quiz").submit(function(event) {
    event.preventDefault()
-   var q1 = $("input : radio [name = q1] : checked").val();
-   var q2 = $("input :radio [name =q2] : checked").val();
-   var q3 =$("input :radio [name=q3]: checked").val();
-   var q4 =$("input : radio [name=q4]: checked").val();
-   var q5 =$("input : radio [name=q5]: checked").val();
- }
-}
+   var q1 = parseInt($("input:radio[name=q1]:checked").val());
+   var q2 = parseInt($("input:radio[name=q2]:checked").val());
+   var q3 =parseInt($("input:radio[name=q3]:checked").val());
+   var q4 =parseInt($("input:radio[name=q4]:checked").val());
+   var q5 =parseInt($("input:radio[name=q5]:checked").val());
+
+
+
+   var total=q1+q2+q3+q4+q5;
+   $("#output").text("Your Answer is " + total + " out of 10");
+ });
+});
